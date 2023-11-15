71dbc888fdced434946ab22c535b21728c941c03
August Salimov <38182511+augustsalimov@users.noreply.github.com>
2023-11-15 22:06:13 +0100

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,19 @@
+// Напечатать все числа с двузначной суммой цифр в диапазоне от 40 до 70.
+// [40, 70) от 40 включительно до 70 (исключая 70)
+
+function summOfDigits(num) {
+    var sum = 0;
+    var stringifyNum = num.toString();
+    for (var i = 0; i < stringifyNum.length; i++) sum += +stringifyNum[i];
+    return sum;
+}
+
+const arrayOfNums = (first, last) => {
+    let nums = [];
+    while (first !== last) {
+        if (summOfDigits(first).toString().length == 2) nums.push(first); first++;
+    };
+    return nums;
+}
+
+console.log(arrayOfNums(40, 70))
